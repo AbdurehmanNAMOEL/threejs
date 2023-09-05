91cd251273484026e2ef014ce7345f1b0fe9b84b
Abdurehman Saeed <asaeed526@gmail.com>
2023-09-05 17:45:29 +0300

    loading animation added

--- src/components/LoadingPageAnimation.jsx
+++ src/components/LoadingPageAnimation.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import spaceLogo from '../assets/space_exploration.jpg'
+import LoadingAnimation from './LoadingAnimation'
 const LoadingPageAnimation = () => {
   return (
-    <div className='bg-[#121212] w-full h-[100vh] fixed z-[10000] flex justify-center items-center'>
+    <div className='bg-[#121212] w-full h-[100vh] fixed z-[10000] flex justify-center items-center flex-col gap-4'>
       <img
        className='w-[80px] h-[80px] rounded-full shadow-md' 
        src={spaceLogo} 
        alt='logo'/>
+       <LoadingAnimation/>
     </div>
   )
 }
